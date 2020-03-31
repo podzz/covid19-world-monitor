a62620bd5fe262a8720f4a13b3e4af823593310b
podzz <francois.boiteux@epita.fr>
2020-03-31 21:49:09 +0000

    Deploying to gh-pages from master 229e8d96ea9e11f509fb610125a3df0f735e52e4

---
+++ precache-manifest.86a68487bd9c12c843eef6a653b14052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0572b872049b4c534fad313828fd17d",
+    "url": "/covid19-world-monitor/index.html"
+  },
+  {
+    "revision": "58c03ef129a94478e201",
+    "url": "/covid19-world-monitor/static/css/main.e7f0f4f3.chunk.css"
+  },
+  {
+    "revision": "5ed6032746842e947217",
+    "url": "/covid19-world-monitor/static/js/2.a7d3e665.chunk.js"
+  },
+  {
+    "revision": "cbe8b87f31049a689db07e5c6401420a",
+    "url": "/covid19-world-monitor/static/js/2.a7d3e665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c03ef129a94478e201",
+    "url": "/covid19-world-monitor/static/js/main.06ecf981.chunk.js"
+  },
+  {
+    "revision": "4c164bb539a13a20dce8",
+    "url": "/covid19-world-monitor/static/js/runtime-main.13d47124.js"
+  }
+]);
