1110cd01759eef87e6708c690fea06f6a62f7a25
Francois Boiteux <francois_boiteux@amp.com.au>
2020-04-07 21:07:14 +1000

    fix datasource

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
 import createSagaMiddleware from "redux-saga";
 import thunk from "redux-thunk";
 import App from "./App";
 import "./index.css";
 import { mapCasesReducer } from "./redux/reducers/mapCases.reducer";
+import { mapPolygonsReducer } from "./redux/reducers/mapPolygons.reducer";
 import { watchMapCases, watchMapPolygons } from "./redux/sagas";
 import * as serviceWorker from "./serviceWorker";
-import { mapPolygonsReducer } from "./redux/reducers/mapPolygons.reducer";
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = applyMiddleware(thunk, sagaMiddleware);
